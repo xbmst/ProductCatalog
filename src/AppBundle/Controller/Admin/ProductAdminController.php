a99555d2634e66500bf2bc176d0ef65f3fc3d58f
xbmst <tsymbik@gmail.com>
2016-11-15 02:07:35 +0300

    Added features: -add new product -list all products

---
+++ src/AppBundle/Controller/Admin/ProductAdminController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use AppBundle\Entity\Product;
+use AppBundle\Form\ProductType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/admin")
+ */
+class ProductAdminController extends Controller
+{
+    /**
+     * @Route("/product", name="admin_product_list")
+     */
+    public function indexAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->findAll();
+
+        return $this->render('admin/product/list.html.twig', array(
+            'products' => $products
+        ));
+    }
+
+    /**
+     * @Route("/product/new", name="admin_product_new")
+     */
+    public function newAction(Request $request)
+    {
+        $form = $this->createForm(ProductType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Product added!');
+
+            return $this->redirectToRoute('admin_product_list');
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'productForm' => $form->createView()
+        ]);
+    }
+}
