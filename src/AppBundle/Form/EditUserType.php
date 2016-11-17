1d0f6f0e6bd5548774a94883814e58ce7ae0c968
xbmst <tsymbik@gmail.com>
2016-11-17 03:51:45 +0300

    Roles testing succeed. Roles work (check security.yml and controllers annotations). Added user editing (/admin/user).

--- src/AppBundle/Form/EditUserType.php
+++ src/AppBundle/Form/EditUserType.php
@@ -2,35 +2,34 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bridge\Doctrine\Security\User\EntityUserProvider;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EditUserFormType extends AbstractType
+class EditUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('email', EmailType::class)
             ->add('username', TextType::class)
-            ->add('password', PasswordType::class)
-            ->add('roles', EntityType::class,[
-                'class' => User::class,
-
+            ->add('isActive', ChoiceType::class, [
+                'choices' => [
+                    'Yes' => true,
+                    'No' => false,
+                ]
+            ])
+            ->add('roles', ChoiceType::class,[
+                'multiple' => true,
+                'expanded' => true,
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Moderator' => 'ROLE_MOD',
+                    'User' => 'ROLE_USER'
+                ]
             ]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\User',
-        ));
     }
 }
