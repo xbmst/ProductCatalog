b4578763b8984a624580a60851f08320ca0a1b79
xbmst <tsymbik@gmail.com>
2016-11-28 15:39:38 +0300

    Merged with exymax

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -17,23 +17,34 @@ class Builder implements ContainerAwareInterface
 
         $menu->addChild('Home', ['route' => 'homepage']);
 
-        $em = $this->container->get('doctrine')->getManager();
-        $category = $em->getRepository('AppBundle:ProductCategory')->findAll();
-
         $menu->addChild('Products', [
             'route' => 'products_all',
             ]);
 
-        $menu['Products']->addChild('Add product', [
-            'route' => 'admin_product_new',
-
-            ]);
-
-        $menu->addChild('Users', [
-           'route' => 'admin_user_list',
+        $menu->addChild('Categories', [
+            'route' => 'admin_category_list'
         ]);
 
-        //dump(count($category));
+        $menu->addChild('Users', [
+            'route' => 'admin_user_list',
+        ]);
+        return $menu;
+    }
+
+    public function modMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('Home');
+
+        $menu->addChild('Home', ['route' => 'homepage']);
+
+        $menu->addChild('Products', [
+            'route' => 'products_all',
+        ]);
+
+        $menu['Products']->addChild('Add product', [
+            'route' => 'admin_product_new',
+        ]);
+
         $menu->addChild('Categories', [
             'route' => 'admin_category_list'
         ]);
