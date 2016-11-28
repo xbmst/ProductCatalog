848de51e2bf24d1e1b2ab355125210901ad72ada
xbmst <tsymbik@gmail.com>
2016-11-28 05:32:11 +0300

    Added product details page and modal skeletons. Details page opens on product name click (route is /admin/product/{id}/show). Details modal opens on product id click.

--- src/AppBundle/Controller/Admin/ProductAdminController.php
+++ src/AppBundle/Controller/Admin/ProductAdminController.php
@@ -81,4 +81,23 @@ class ProductAdminController extends Controller
             'productForm' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Security("is_granted('ROLE_MANAGE_PRODUCT')")
+     * @Route("/product/{id}/show", name="admin_product_show")
+     */
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        return $this->render('admin/product/show.html.twig', array(
+            'product' => $product,
+        ));
+    }
 }
